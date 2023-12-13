699a3cfcfec814789ad3e4361eaf71c6cf9719d7
Rafael Macchi <rafaelmacchi199@gmail.com>
2023-12-13 18:36:11 -0300

    feat: Include authenticate JWT for users

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { env } from 'src/shared/config/env';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      signOptions: { expiresIn: '2d' },
+      secret: env.jwtSecret,
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
