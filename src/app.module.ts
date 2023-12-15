0f8ee3c92d6c4357625e62ebed77e9618817af79
Rafael Macchi <rafaelmacchi199@gmail.com>
2023-12-15 20:09:47 -0300

    feat: Include AuhtGuard in providers and add JwtService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { DatabaseModule } from './shared/database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './modules/auth/auth.guard';
+
+import { JwtService } from '@nestjs/jwt';
+
 @Module({
   imports: [UsersModule, DatabaseModule, AuthModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    JwtService,
+  ],
 })
 export class AppModule {}
