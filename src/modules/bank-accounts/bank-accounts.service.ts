f001f14ee17a6f33e76d29094c9ae519d02a22f7
Rafael Macchi <rafaelmacchi199@gmail.com>
2023-12-17 21:25:40 -0300

    feat: Create Bank-accounts resource

---
+++ src/modules/bank-accounts/bank-accounts.service.ts
@@ -0,0 +1,71 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateBankAccountDto } from './dto/create-bank-account.dto';
+import { UpdateBankAccountDto } from './dto/update-bank-account.dto';
+import { BankAccountsRepository } from 'src/shared/database/repositories/bank-accounts.repositories';
+
+@Injectable()
+export class BankAccountsService {
+  constructor(private readonly bankAccountsRepo: BankAccountsRepository) {}
+
+  create(userId: string, createBankAccountDto: CreateBankAccountDto) {
+    const { color, initialBalance, name, type } = createBankAccountDto;
+
+    return this.bankAccountsRepo.create({
+      data: {
+        userId,
+        color,
+        initialBalance,
+        name,
+        type,
+      },
+    });
+  }
+
+  findAllByUserId(userId) {
+    return this.bankAccountsRepo.findMany({
+      where: { userId },
+    });
+  }
+
+  async update(
+    userId: string,
+    bankAccountId: string,
+    updateBankAccountDto: UpdateBankAccountDto,
+  ) {
+    const isOwner = await this.bankAccountsRepo.findFirst({
+      where: { id: bankAccountId, userId },
+    });
+
+    if (!isOwner) {
+      throw new NotFoundException('Bank account not found.');
+    }
+
+    const { color, initialBalance, name, type } = updateBankAccountDto;
+
+    return this.bankAccountsRepo.update({
+      where: { id: bankAccountId },
+      data: {
+        color,
+        initialBalance,
+        name,
+        type,
+      },
+    });
+  }
+
+  async remove(userId: string, bankAccountId: string) {
+    const isOwner = await this.bankAccountsRepo.findFirst({
+      where: { id: bankAccountId, userId },
+    });
+
+    if (!isOwner) {
+      throw new NotFoundException('Bank account not found.');
+    }
+
+    await this.bankAccountsRepo.delete({
+      where: { id: bankAccountId },
+    });
+
+    return null;
+  }
+}
