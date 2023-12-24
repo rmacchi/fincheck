edf421b31bb77ce747589e5908e16f7d352349ea
Rafael Macchi <rafaelmacchi199@gmail.com>
2023-12-23 21:21:17 -0300

    feat: Checking if the bankAccount and category belong to the corresponding user

--- src/modules/transactions/transactions.service.ts
+++ src/modules/transactions/transactions.service.ts
@@ -3,20 +3,24 @@ import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import { TransactionsRepository } from 'src/shared/database/repositories/transactions.repositories';
 import { ValidateBankAccountOwnershipService } from '../bank-accounts/services/validate-bank-account-ownership.service';
+import { ValidateCategoryOwnershipService } from '../categories/services/validate-category-ownership.service';
 
 @Injectable()
 export class TransactionsService {
   constructor(
     private readonly transactionsRepo: TransactionsRepository,
     private readonly validateBankAccountOwnershipService: ValidateBankAccountOwnershipService,
+    private readonly validateCategoryOwnershipService: ValidateCategoryOwnershipService,
   ) {}
 
   async create(userId: string, createTransactionDto: CreateTransactionDto) {
-    const { bankAccountId } = createTransactionDto;
-    await this.validateBankAccountOwnershipService.validate(
+    const { bankAccountId, categoryId } = createTransactionDto;
+
+    await this.validateEntitiesOwnerShip({
       userId,
       bankAccountId,
-    );
+      categoryId,
+    });
 
     return 'Deve criar a Transaction';
   }
@@ -33,5 +37,20 @@ export class TransactionsService {
 
   remove(id: number) {
     return `This action removes a #${id} transaction`;
+  }
+
+  private async validateEntitiesOwnerShip({
+    userId,
+    bankAccountId,
+    categoryId,
+  }: {
+    userId: string;
+    bankAccountId: string;
+    categoryId: string;
+  }) {
+    await Promise.all([
+      this.validateBankAccountOwnershipService.validate(userId, bankAccountId),
+      this.validateCategoryOwnershipService.validate(userId, categoryId),
+    ]);
   }
 }
