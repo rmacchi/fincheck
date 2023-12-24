a86836b30d48f9b909be4634f2cb61c974ce7160
Rafael Macchi <rafaelmacchi199@gmail.com>
2023-12-23 21:17:14 -0300

    feat: Imported the CategoriesModule

--- src/modules/transactions/transactions.module.ts
+++ src/modules/transactions/transactions.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
 import { BankAccountsModule } from '../bank-accounts/bank-accounts.module';
+import { CategoriesModule } from '../categories/categories.module';
 
 @Module({
-  imports: [BankAccountsModule],
+  imports: [BankAccountsModule, CategoriesModule],
   controllers: [TransactionsController],
   providers: [TransactionsService],
 })
