ceaefa38a2e15161af24ce4f82578b3f96a63e41
Rafael Macchi <rafaelmacchi199@gmail.com>
2024-02-04 23:12:39 -0300

    Creating application Dropdowns and Modals with Radix UI

---
+++ frontend/src/view/pages/Dashboard/components/Transactions/index.tsx
@@ -0,0 +1,139 @@
+import { FilterIcon } from "../../../../components/icons/FilterIcon";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { MONTHS } from "../../../../../app/config/constants";
+import { SliderOption } from "./SliderOption";
+import { SliderNavigation } from "./SliderNavigation";
+import { formatCurrency } from "../../../../../app/utils/formatCurrency";
+import { CategoryIcon } from "../../../../components/icons/categories/CategoryIcon";
+import { cn } from "../../../../../app/utils/cn";
+import { useTransactionsController } from "./useTransactionsController";
+import { Spinner } from "../../../../components/Spinner";
+import emptyStateImage from "../../../../../assets/empty-state.svg"
+import { TransactionTypeDropdown } from "./TransactionTypeDropdown";
+import { FiltersModal } from "./FiltersModal";
+
+export function Transactions() {
+  const {
+    areValuesVisible,
+    isInitialLoading,
+    transactions,
+    isLoading,
+    isFiltersModalOpen,
+    handleCloseFiltersModal,
+    handleOpenFiltersModal,
+  } = useTransactionsController();
+
+  const hasTransactions = transactions.length > 0;
+
+  return (
+    <div className="bg-gray-100 rounded-2xl w-full h-full p-10 flex flex-col">
+      {isInitialLoading && (
+        <div className="w-full h-full flex items-center justify-center">
+          <Spinner className="w-10 h-10 text-gray-400" />
+        </div>
+      )}
+
+      {!isInitialLoading && (
+        <>
+          <FiltersModal
+            open={isFiltersModalOpen}
+            onClose={handleCloseFiltersModal}
+          />
+
+          <header>
+            <div className="flex items-center justify-between">
+              <TransactionTypeDropdown />
+
+              <button onClick={handleOpenFiltersModal}>
+                <FilterIcon />
+              </button>
+            </div>
+
+            <div className="mt-6 relative">
+              <Swiper
+                slidesPerView={3}
+                centeredSlides
+              >
+                <SliderNavigation />
+
+                {MONTHS.map((month, index) => (
+                  <SwiperSlide key={month}>
+                    {({ isActive }) => (
+                      <SliderOption
+                        isActive={isActive}
+                        month={month}
+                        index={index}
+                      />
+                    )}
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+          </header>
+
+          <div className="mt-4 space-y-2 flex-1 overflow-y-auto">
+            {isLoading && (
+              <div className="flex flex-col items-center justify-center h-full">
+                <Spinner className="w-10 h-10" />
+              </div>
+            )}
+
+            {(!hasTransactions && !isLoading) && (
+              <div className="flex flex-col items-center justify-center h-full">
+                <img src={emptyStateImage} alt="Empty State" />
+                <p className="text-gray-700">
+                  Não encontramos nenhuma transação!
+                </p>
+              </div>
+            )}
+
+            {(hasTransactions && !isLoading) && (
+              <>
+                <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
+                  <div className="flex-1 flex items-center gap-3">
+                    <CategoryIcon type="expense" />
+
+                    <div>
+                      <strong className="font-bold tracking-[-0.5px] block">Almoço</strong>
+                      <span className="text-sm text-gray-600">04/01/2024</span>
+                    </div>
+                  </div>
+
+                  <span
+                    className={cn(
+                      "text-red-800 tracking-[-0.5px] font-medium",
+                      !areValuesVisible && 'blur-sm'
+                    )}
+                  >
+                    - {formatCurrency(449)}
+                  </span>
+                </div>
+
+                <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
+                  <div className="flex-1 flex items-center gap-3">
+                    <CategoryIcon type="income" />
+
+                    <div>
+                      <strong className="font-bold tracking-[-0.5px] block">Freelance</strong>
+                      <span className="text-sm text-gray-600">20/01/2024</span>
+                    </div>
+                  </div>
+
+                  <span
+                    className={cn(
+                      "text-green-800 tracking-[-0.5px] font-medium",
+                      !areValuesVisible && 'blur-sm',
+                    )}
+                  >
+                    {formatCurrency(7200)}
+                  </span>
+                </div>
+              </>
+            )}
+
+          </div>
+        </>
+      )}
+    </div>
+  )
+}
